9a0736d20607f4ace4a982107d5995f00b154c30
Harshdeep7624 <tegh293295@gmail.com>
2025-04-01 14:04:14 +0530

    Update xyz.sol

--- xyz.sol
+++ xyz.sol
@@ -1,1 +1,19 @@
+pragma solidity ^0.8.0;
+
+contract ScavengerHunt {
+    address public owner;
+    mapping(address => bool) public winners;
+    
+    event WinnerDeclared(address winner);
+    
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function declareWinner(address _winner) public {
+        require(msg.sender == owner, "Not authorized");
+        winners[_winner] = true;
+        emit WinnerDeclared(_winner);
+    }
+}
 
